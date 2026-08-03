df8edaf54fd37180bbecf5b010716c5f39d07b23
Yaroslav Lapin <ylapin@legalzoom.com>
2026-08-03 14:22:43 -0600

    fix(app): remove Park app route

--- packages/app/src/app/park.native.tsx
+++
@@ -1,7 +0,0 @@
-import { useLocalSearchParams } from "expo-router";
-import { ParkPage, type ParkPageParams } from "@/park-page";
-
-export default function ParkRoute() {
-  const params = useLocalSearchParams() as ParkPageParams;
-  return <ParkPage params={params} />;
-}
